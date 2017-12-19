5bf97337ff00e62c1073f3ee9b3979385d26e255
luuk382 <l.a.roozen@student.tue.nl>
2017-12-19 11:49:14 +0100

    second commit

--- web2venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\s155444\desktop\school\web-science\2th year\2th quartile\2id60-web technology\web2venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
